fab56fd684bb7485f325dbe1b1ddbdaa3f723989
lucas-r-nogueira <ramalhol148@hotmail.com>
2024-02-23 10:06:35 -0300

    feature body responsive

--- Planet Fact/src/components/Header/Header.jsx
+++ Planet Fact/src/components/Header/Header.jsx
@@ -1,7 +1,17 @@
+import React, { useState } from 'react';
 import styles from "./Header.module.css";
+import 'hamburgers/dist/hamburgers.css';
+import '../../assets/styles/hamburguers.css';
 
 
 export default function Header({ numberPlanet }) {
+
+  const [menuAberto, setMenuAberto] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuAberto(!menuAberto);
+
+  };
 
   const handleClick = (numeroPlaneta) => {
     numberPlanet(numeroPlaneta);
@@ -14,30 +24,35 @@ export default function Header({ numberPlanet }) {
           <h1>The Planets</h1>
         </div>
         <div className={styles.container__header_nav}>
+          <button className={`hamburger hamburger--spin ${menuAberto ? 'is-active' : ''}`} type="button" onClick={toggleMenu}>
+            <span className="hamburger-box">
+              <span className="hamburger-inner"></span>
+            </span>
+          </button>
           <ul>
             <li>
-              <a onClick={()=> handleClick(0)}>Mercury</a>
+              <a onClick={() => handleClick(0)}>Mercury</a>
             </li>
             <li>
-              <a onClick={()=> handleClick(1)}>Venus</a>
+              <a onClick={() => handleClick(1)}>Venus</a>
             </li>
             <li>
-              <a onClick={()=> handleClick(2)}>Earth</a>
+              <a onClick={() => handleClick(2)}>Earth</a>
             </li>
             <li>
-              <a onClick={()=> handleClick(3)}>Mars</a>
+              <a onClick={() => handleClick(3)}>Mars</a>
             </li>
             <li>
-              <a onClick={()=> handleClick(4)}>Jupiter</a>
+              <a onClick={() => handleClick(4)}>Jupiter</a>
             </li>
             <li>
-              <a onClick={()=> handleClick(5)}>Saturn</a>
+              <a onClick={() => handleClick(5)}>Saturn</a>
             </li>
             <li>
-              <a onClick={()=> handleClick(6)}>Uranus</a>
+              <a onClick={() => handleClick(6)}>Uranus</a>
             </li>
             <li>
-              <a onClick={()=> handleClick(7)}>Neptune</a>
+              <a onClick={() => handleClick(7)}>Neptune</a>
             </li>
           </ul>
         </div>
--- Planet Fact/src/main.jsx
+++ Planet Fact/src/main.jsx
@@ -4,7 +4,6 @@ import App from './App.jsx'
 import './assets/styles/global.module.css'
 import 'animate.css';
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
